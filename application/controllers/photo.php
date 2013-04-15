e06055e9386e69b5efb6c1f2eae3df32c3c71f5c
Turki Hamza <albert.robert456@gmail.com>
2013-04-15 14:57:33 +0100

    Ajout des fonctionnalités photos et statut (ajout, modification, suppression, affichage)

---
+++ application/controllers/photo.php
@@ -0,0 +1,88 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Photo extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->twig->addFunction('getsessionhelper');
+        $this->load->library('form_validation');
+        $this->load->model('photo_model', 'photoManager');
+        $this->load->model('statut_model', 'statutManager');
+    }
+        
+    public function index($categorie = "")
+    {
+        $data = array();
+        
+        if(empty($categorie))
+        {
+            $data['res'] = $this->photoManager->getAllPhotos(getsessionhelper()['id']);
+            foreach($data['res'] as $value)
+            {
+                $value->statut = $this->statutManager->getStatut($value->statut)->statut;
+                
+                if($value->partage == 0)
+                    $value->etat = 'Image non partagé';
+                else
+                    $value->etat = 'Image partagé';
+            }
+            
+            $this->twig->render('photo_view', $data);
+        }
+        else
+        {
+            $data['res'] = $this->photoManager->getPhotosByCategorie(getsessionhelper()['id'], $categorie);
+            foreach($data['res'] as $value)
+            {
+                $value->statut = $this->statutManager->getStatut($value->statut);
+            }
+            $this->twig->render('photo_view', $data);
+        }
+        
+    }
+    
+    public function categorie()
+    {
+        $this->twig->render('categorie_view');
+    }
+    
+    public function modifier($id)
+    {
+        if(empty($id))
+            exit('Erreur ID Photo');
+        
+        $data = array();
+        $data['s'] = $this->statutManager->getStatut($this->photoManager->getPhoto($id)->statut)->statut;
+        $data['l'] = $this->photoManager->getPhoto($id)->localisation;
+        
+        //$this->form_validation->set_error_delimiters('<p class="form_erreur">', '</p>');
+        
+        $this->form_validation->set_rules('localisation', '\'Localisation\'', 'trim|required|max_length[255]|xss_clean');
+        $this->form_validation->set_rules('statut', '\'Statut\'', 'trim|required|xss_clean');
+        
+        if($this->form_validation->run())
+        {
+            $this->photoManager->updatephoto($id);
+            redirect('/photo');
+        }
+        else
+        {
+            $this->twig->render('modifier_photo_view', $data);
+        }
+    }
+
+        public function supprimer($id)
+    {
+        if(empty($id))
+            exit('Erreur ID image');
+        
+        $this->photoManager->deletePhoto($id);
+        redirect('/photo');
+    }
+    
+}
+
+?>
+
+
---
+++ application/models/statut_model.php
@@ -0,0 +1,58 @@
+<?php
+class Statut_model extends CI_Model
+{
+    private $table = 'statut';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public function getStatut($id)
+    {
+        return $this->db->get_where($this->table, array('id' => $id))->row();
+    }
+    
+    public function getAllNonAttachStatus($id)
+    {
+        return $this->db->where('user', $id)
+                 ->where('attache', 0)
+                 ->order_by('date_envoi', 'desc')
+                 ->get($this->table)
+                 ->result();
+    }
+    
+    public function addStatut()
+    {
+        $data = array('statut' => $this->input->post('statut'),
+                      'localisation' => $this->input->post('localisation'),
+                      'date_envoi' => date('Y-m-d H:i:s'),
+                      'partage' => 0,
+                      'attache' => 0,
+                      'user' => getsessionhelper()['id']);
+
+        $this->db->insert($this->table, $data);
+    }
+
+    public function updateStatut($id)
+    {
+        if(empty($id))
+            return false;
+        
+        $data = array('localisation' => $this->input->post('localisation'),
+                      'statut' => $this->input->post('statut'),
+                      'date_envoi' => date('Y-m-d H:i:s'));
+        $this->db->update($this->table, $data, array('id' => $id));
+        return true;
+    }
+
+        public function deleteStatut($id)
+    {
+        $this->db->delete($this->table, array('id' => $id));
+    }
+
+}
+
+?>
+
+
